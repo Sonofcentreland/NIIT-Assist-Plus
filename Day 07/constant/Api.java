380081412278697559ca005b17096770c9fe229b
Sonofcentreland <110534759+Sonofcentreland@users.noreply.github.com>
2024-03-27 10:45:06 +0530

    Add files via upload
    day 07

---
+++ Day 07/constant/Api.java
@@ -0,0 +1,14 @@
+package com.jeeva.constant;
+
+public class Api {
+
+    public static final String VITE = "http://localhost:5173";
+    public static final String AUTH = "/niit/auth";
+    public static final String USER = "/niit/user";
+    public static final String COURSE = "/niit/course";
+    public static final String ENROLL = "/niit/enroll";
+    public static final String ENQUIRY = "/niit/enquiry";
+    public static final String PAYMENT = "/niit/payment";
+    public static final String MAIL = "/niit/mail";
+
+}
